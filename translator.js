1c023ceca7854d2a4bc2139785e3731703941a1b
3an9yu <3an9yu@gmail.com>
2017-01-10 17:40:24 -0600

    Validate id

--- translator.js
+++ translator.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
 	function id_to_num(pad_id) {
-		var a = pad_id.charAt(0);
-		var b = pad_id.charAt(1);
-		var c = pad_id.charAt(2);
-		var d = pad_id.charAt(3);
-		var e = pad_id.charAt(4);
-		var f = pad_id.charAt(5);
-		var g = pad_id.charAt(6);
-		var h = pad_id.charAt(7);
-		var i = pad_id.charAt(8);
+		if (/^[0-9]{9}$/.test(pad_id)) {
+			if (parseInt(pad_id) >= 396534278) {
+				var a = pad_id.charAt(0);
+				var b = pad_id.charAt(1);
+				var c = pad_id.charAt(2);
+				var d = pad_id.charAt(3);
+				var e = pad_id.charAt(4);
+				var f = pad_id.charAt(5);
+				var g = pad_id.charAt(6);
+				var h = pad_id.charAt(7);
+				var i = pad_id.charAt(8);
 
-		var code = parseInt(a + g + e + h + b + d + i + f + c);
-		code -= 323795845;
+				var code = parseInt(a + g + e + h + b + d + i + f + c);
+				code -= 323795845;
 
-		return code.toString();
+				return code.toString();
+			} else {
+				return "Invalid ID";
+			}
+		} else {
+			return "Invalid ID";
+		}
 	}
 
 	function num_to_id(pad_number) {
